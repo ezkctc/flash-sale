823a8134c39e32e6c1080dfbcbd4c141aaf61466
ezkctc <46705852+ezkctc@users.noreply.github.com>
2025-10-30 23:13:07 +0800

    Updated layout.tsx

--- apps/site-web/src/app/layout.tsx
+++ apps/site-web/src/app/layout.tsx
@@ -1,6 +1,23 @@
 import { FlashSalePage } from '../components/FlashSalePage';
 
 export default function Index() {
+  return (
+    <div>
+      <FlashSalePage />
+      <div>
+        <div>
+          <div>
+            <details>
+              <summary>
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     strokeWidth="2"
                     d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
